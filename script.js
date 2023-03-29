22f482b68e20cf39a8564cf1f6a7bca012d9e593
[Hardik Nanda] <[hnanda005@gmail.com]>
2023-03-29 07:34:03 +0530

    Adding modified files with css

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { json } = require('express');
-const hbs = require('hbs');
-const path = require('path');
-const partialsPath = path.join(__dirname, './views/partials');
+const { json } = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const partialsPath = path.join(__dirname, "./views/partials");
 
-app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 hbs.registerPartials(partialsPath);
 
-app.use('/patient', require('./routes/patient'));
+app.use("/patient", require("./routes/patient"));
 
-app.get('/', (req, res) => {
-    res.render('./homePage');
-})
+app.get("/", (req, res) => {
+  res.render("./homePage");
+});
 
 app.listen(PORT, () => {
-    console.log('Server started at: ' + PORT);
-})
+  console.log("Server started at: " + PORT);
+});
